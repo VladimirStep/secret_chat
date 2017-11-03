aec70b7c65d9e53691e2f82977e8271cb702f8c7
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-03 14:59:25 +0200

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has valid factory' do
+    expect(build(:user)).to be_valid
+  end
+
+  let(:current_user) { build(:user) }
+
+  subject { current_user }
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('not-an-email').for(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_length_of(:password).is_at_most(128) }
+  end
+
+  describe 'database' do
+    it { should have_db_index(:email).unique(:true) }
+    it { should have_db_column(:provider).of_type(:string) }
+    it { should have_db_column(:uid).of_type(:string) }
+  end
+
+  describe 'associations' do
+    it { should have_one(:profile) }
+    it { should have_many(:chat_rooms) }
+  end
 end
