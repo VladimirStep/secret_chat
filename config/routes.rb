96996d622c71ac331da4dd32c79801a94f7caa09
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-06 09:23:27 +0200

    Generate devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'chat_rooms#index'
+
+  scope 'api/v1' do
+    devise_for :users, defaults: { format: :json }
+  end
 end
