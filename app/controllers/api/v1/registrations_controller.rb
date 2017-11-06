e2682e3cf80d08d96b078f52dea93b507d2fd9f4
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-06 12:59:28 +0200

    Add specs and controller action for create user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -8,9 +8,26 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        render json: resource
+      else
+        expire_data_after_sign_in!
+        render json: resource
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      render json: { errors: resource.errors.full_messages},
+             status: :unprocessable_entity
+    end
+  end
 
   # GET /resource/edit
   # def edit
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'Requests to Users controller', type: :request do
+  context 'with correct params' do
+    let(:user_attributes) { attributes_for(:user)
+                              .merge(password_confirmation: 'password') }
 
+    it 'should create new user' do
+      post '/api/v1/users',
+           params: { user: user_attributes }.to_json,
+           headers: json_headers
+      user = User.last
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(:success)
+      expect(json).to eq({
+                           user: {
+                             id: user.id,
+                             email: user.email,
+                             token: JWTCoder.encode(user_id: user.id)
+                           }
+                         })
+    end
+  end
+
+  context 'with wrong params' do
+    let(:wrong_attributes) { attributes_for(:user)
+                              .merge(password_confirmation: 'wrong') }
+
+    it 'should return errors' do
+      post '/api/v1/users',
+           params: { user: wrong_attributes }.to_json,
+           headers: json_headers
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json).to eq({
+                            errors: ["Password confirmation doesn't match Password"]
+                         })
+    end
+  end
 end
